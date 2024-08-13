b27cf97467554c1f0b893f079f592f69c111e53f
Siddhesh <siddheshdilipkulkarni@gmail.com>
2024-08-13 14:54:43 +0530

    SingleChildScrollView commit

---
+++ lib/color.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color netflixColor =  Color(0xFFd32d29);
+  static const Color netflixAppBarColor =  Color(0xFF474749);
+
+}
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_demo_project/color.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({super.key});
@@ -13,12 +14,38 @@ class _DashboardState extends State<Dashboard> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
+        title: Container(
+          color: AppColors.netflixAppBarColor,
+          child: Row(
+            children: [
+              IconButton(onPressed: (){}, icon: Icon(
+                Icons.menu,
+                color: Colors.white,
+              )),
+              Spacer(),
+              Text("Netflix",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: AppColors.netflixColor,
+                  fontSize: 24
+                ),
+              ),
+              Spacer(),
+              IconButton(onPressed: (){}, icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+            ],
+          ),
+        ),
       ),
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
 
+          ],
+        ),
       ),
     );
   }
 }
-
