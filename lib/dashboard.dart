1103638cc2c059d8784b7ea73eef26b7cc27dea8
Siddhesh <siddheshdilipkulkarni@gmail.com>
2024-08-13 13:45:50 +0530

    Movie design commit

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -9,54 +9,16 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  List ofXO = List.generate(9, (index) => "");
-  bool whosTurn = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GridView(
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              crossAxisSpacing: 10,
-              mainAxisSpacing: 10
-          ),
-          children: [
-            for(int i = 0; i < 9; i++)...[
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    if (ofXO[i] == "")
-                      if (whosTurn) {
-                        ofXO[i] = "X";
-                        whosTurn = false;
-                      }
-                      else {
-                        ofXO[i] = "0";
-                        whosTurn = true;
-                      }
-                    setState(() {});
-                  },
-                  child: Container(
-                    alignment: Alignment.center,
-                    color: Colors.amber,
-                    child: Text(
-                      ofXO[i],
-                      style: TextStyle(
-                          fontSize: 35,
-                          color: Colors.black
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ]
-          ],
-        ),
+      appBar: AppBar(
+
+      ),
+      body: Column(
+
       ),
     );
   }
 }
+
