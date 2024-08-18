a21c5400d91dffb89f9c76df58defabc57bd02dc
Siddhesh <siddheshdilipkulkarni@gmail.com>
2024-08-18 11:18:09 +0530

    Scrollable widgets assignment

---
+++ lib/lay_outs.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LayoutsPrac extends StatefulWidget {
+  const LayoutsPrac({super.key});
+
+  @override
+  State<LayoutsPrac> createState() => _LayoutsPracState();
+}
+
+class _LayoutsPracState extends State<LayoutsPrac> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: Text("Layouts"),
+      ),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for(int i= 0; i<70;i++) ...[
+                  Container(
+                    height: 70,
+                    width: 70,
+                    color: i%2 ==0 ? Colors.red : Colors.amber,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ]
+              ],
+
+            ),
+          ),
+          Expanded(child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10
+            ),
+            children: [
+              for(int i = 0; i<= 50;i++)...[
+                Container(
+                  width: 150,
+                  height: 150,
+                  color: i%2 ==0 ? Colors.red : Colors.amber,
+                )
+              ]
+            ],
+          )),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for(int i= 0; i<70;i++) ...[
+                  Container(
+                    height: 70,
+                    width: 70,
+                    color: i%2 ==0 ? Colors.red : Colors.amber,
+                  ),
+                  SizedBox(
+                    width: 10,
+                  )
+                ]
+              ],
+
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo_project/dashboard.dart';
+import 'package:flutter_demo_project/lay_outs.dart';
+import 'package:flutter_demo_project/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,22 +19,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Dashboard(),
+      home: const SplashScreen(),
     );
   }
 }
 
-class TicTacToGame extends StatefulWidget {
-  const TicTacToGame({super.key});
-
-  @override
-  State<TicTacToGame> createState() => _TicTacToGameState();
-}
-
-class _TicTacToGameState extends State<TicTacToGame> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
 
