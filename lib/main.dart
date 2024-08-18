da746a9808a5601a8b4a33eacc412d618ca93e10
Siddhesh <siddheshdilipkulkarni@gmail.com>
2024-08-18 15:27:29 +0530

    Assets commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo_project/dashboard.dart';
+import 'package:flutter_demo_project/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,22 +18,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Dashboard(),
+      home: const LoginScreen(),
     );
   }
 }
 
-class TicTacToGame extends StatefulWidget {
-  const TicTacToGame({super.key});
 
-  @override
-  State<TicTacToGame> createState() => _TicTacToGameState();
-}
-
-class _TicTacToGameState extends State<TicTacToGame> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
 
